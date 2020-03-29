aa7afd2826fad6015fff13a0e701872f6145e24e
Genesis Pereira <profgenesislima@gmail.com>
2020-03-28 22:06:43 -0300

    changing jstl implementation to thymeleaf template engine.

--- src/main/java/gl/edu/ifpb/tt/ecommerce/cart/ShoppingCart.java
+++ src/main/java/gl/edu/ifpb/tt/ecommerce/cart/ShoppingCart.java
@@ -3,19 +3,38 @@ package gl.edu.ifpb.tt.ecommerce.cart;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import gl.edu.ifpb.tt.ecommerce.entity.Product;
 import gl.edu.ifpb.tt.ecommerce.exceptions.InvalidProductIdException;
+import gl.edu.ifpb.tt.ecommerce.service.ProductService;
 
+//@Controller
+//@RequestMapping("cart")
 public class ShoppingCart {
 
 	private List<Item> cart = new ArrayList<Item>();
+	
+	@Autowired
+	ProductService productService;
 	
 	public void addItem(Item item) {		
 		this.cart.add(item);
 	}
 	
-	public void removeItem(Item item) {
-		this.cart.remove(item);
+	@RequestMapping(value="remove/{id}",method = RequestMethod.GET)
+	public String removeItem(@PathVariable("id") String id, HttpSession session) {
+		
+		List<Item> cart = (List<Item>) session.getAttribute("cart");
+		int index = this.alreadyExists(id, cart);
+		cart.remove(index);
+		session.setAttribute("cart", cart);
+		return "redirect:/cart/index";
 	}
 
 	public void updateItem(Item item) throws InvalidProductIdException {
@@ -35,6 +54,35 @@ public class ShoppingCart {
 	public Item getItemByProduct(Product product) {
 		return this.cart.stream().
 				filter(i->i.getProduct() == product).findAny().get();
+	}
+	
+	@RequestMapping("comprar/{id}")
+	public String buyItem(@PathVariable("id") String id, HttpSession session) {
+		if(session.getAttribute("cart") == null) {
+			List<Item> shoppingCart = (List<Item>) session.getAttribute("cart");
+			cart.add(new Item(productService.getById(Long.parseLong(id)), 1));
+			session.setAttribute("cart", shoppingCart);
+		}else {
+			List<Item> shoppingCart= (List<Item>) session.getAttribute("cart");
+			int index = this.alreadyExists(id, cart);
+			if (index == -1) {
+				cart.add(new Item(productService.getById(Long.parseLong(id)), 1));
+			} else {
+				int quantity = cart.get(index).getQuantity() + 1;
+				cart.get(index).setQuantity(quantity);
+			}
+			session.setAttribute("cart", cart);
+
+		}
+		return "redirect:/cart/index";
+	}
+	private int alreadyExists(String id, List<Item> cart) {
+		for (int i = 0; i < cart.size(); i++) {
+			if (cart.get(i).getProduct().getId() == Long.parseLong(id)) {
+				return i;
+			}
+		}
+		return -1;
 	}
 	
 }
--- src/main/java/gl/edu/ifpb/tt/ecommerce/controller/CartController.java
+++ src/main/java/gl/edu/ifpb/tt/ecommerce/controller/CartController.java
@@ -15,21 +15,29 @@ import gl.edu.ifpb.tt.ecommerce.cart.Item;
 import gl.edu.ifpb.tt.ecommerce.service.ProductService;
 
 @Controller
+@RequestMapping("/cart")
 public class CartController {
 	
 	@Autowired
 	private ProductService productService;
 	
+	@RequestMapping(value="/index",method = RequestMethod.GET)
+	private String index() {
+		return "/cart/index";
+	}
 	
-	@RequestMapping(value = "/cart/{id}", method = RequestMethod.GET)
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public String buy(@PathVariable("id") String id, HttpSession session) {		
+		System.out.println("ID"+id);
 		if (session.getAttribute("cart") == null) {
 			List<Item> cart = new ArrayList<Item>();
 			cart.add(new Item(productService.getById(Long.valueOf(id)),1));
 			session.setAttribute("cart", cart);
+			System.out.println("CART "+cart.size());
+
 		} else {
 			List<Item> cart = (List<Item>) session.getAttribute("cart");
-			int index = this.exists(Long.valueOf(id), cart);
+			int index = this.exists(Long.parseLong(id), cart);
 			if (index == -1) {
 				cart.add(new Item(productService.getById(Long.valueOf(id)), 1));
 			} else {
@@ -37,6 +45,8 @@ public class CartController {
 				cart.get(index).setQuantity(quantity);
 			}
 			session.setAttribute("cart", cart);
+			System.out.println("CART "+cart.size());
+
 		}
 		return "redirect:/cart/index";
 	}
@@ -49,5 +59,8 @@ public class CartController {
 		}
 		return -1;
 	}
+	
+	
+	
 
 }
